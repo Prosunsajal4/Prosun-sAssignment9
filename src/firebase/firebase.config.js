db141e160cfaec8ba18f7c6e0eb58cc0eb277346
Prosun <prosunsajal123@gmail.com>
2026-01-04 20:44:40 +0600

    Initial commit - Dragon News React App

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBipS-tKVJz_aI6dz-KlqB1IShVJbYhDLc",
+  authDomain: "prosun-auth-as9.firebaseapp.com",
+  projectId: "prosun-auth-as9",
+  storageBucket: "prosun-auth-as9.firebasestorage.app",
+  messagingSenderId: "266165110055",
+  appId: "1:266165110055:web:d98d946de3b19d358a1502",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
