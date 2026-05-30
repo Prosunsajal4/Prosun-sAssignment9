b38c6b0f0efa10dc85d1cd7e7e365e6c032f2b6b
Prosunsajal4 <prosunsajal123@gmail.com>
2026-05-30 19:59:28 +0600

    feat: add ScrollToTop component for route changes

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import Footer from "../pages/Shared/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Root = () => {
   return (
     <div className="flex min-h-screen flex-col font-poppins">
+      <ScrollToTop />
       <a href="#main-content" className="skip-link">
         Skip to main content
       </a>
