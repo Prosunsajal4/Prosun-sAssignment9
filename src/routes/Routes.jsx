14933b154d9e11d493bb52bffe40b774da13c953
Prosunsajal4 <prosunsajal123@gmail.com>
2026-05-30 09:54:46 +0600

    refactor: clean up imports, remove extra .jsx extensions, fix JSX self-closing tags

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,33 +4,48 @@ import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import News from "../pages/News/News";
+import About from "../pages/About/About";
+import Career from "../pages/Career/Career";
+import Addict from "../pages/Addict/Addict";
 import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>,
+    element: <Root />,
     children: [
       {
         path: "/",
-        element: <Home></Home>,
+        element: <Home />,
         loader: () => fetch("/news.json"),
       },
       {
         path: "/news/:id",
         element: (
           <PrivateRoute>
-            <News></News>
+            <News />
           </PrivateRoute>
         ),
       },
       {
         path: "/login",
-        element: <Login></Login>,
+        element: <Login />,
       },
       {
         path: "/register",
-        element: <Register></Register>,
+        element: <Register />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/career",
+        element: <Career />,
+      },
+      {
+        path: "/addict",
+        element: <Addict />,
       },
     ],
   },
