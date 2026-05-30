df93e4190e397a99cf1ae68ede2e7239a8bac2a3
Prosunsajal4 <prosunsajal123@gmail.com>
2026-05-30 09:48:00 +0600

    fix: remove console.log and improve loading state in PrivateRoute

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -5,18 +5,20 @@ import { Navigate, useLocation } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-  console.log(location.pathname);
-  
 
   if (loading) {
-    return <span className="loading loading-infinity loading-lg"></span>;
+    return (
+      <div className="flex min-h-[60vh] items-center justify-center">
+        <span className="loading loading-infinity loading-lg text-indigo-600"></span>
+      </div>
+    );
   }
 
   if (user) {
     return children;
   }
 
-  return <Navigate state={location.pathname} to="/login"></Navigate>;
+  return <Navigate state={location.pathname} to="/login" />;
 };
 
 export default PrivateRoute;
