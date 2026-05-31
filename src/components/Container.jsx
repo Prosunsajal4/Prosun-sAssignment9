5c772d802e7b4fce2af29709b1bb436c3a79b319
Prosunsajal4 <prosunsajal123@gmail.com>
2026-05-31 20:55:37 +0600

    feat: add Container component with size variants

---
+++ src/components/Container.jsx
@@ -0,0 +1,32 @@
+const Container = ({
+  children,
+  size = "default",
+  centered = true,
+  className = "",
+  ...props
+}) => {
+  const sizes = {
+    sm: "max-w-2xl",
+    md: "max-w-4xl",
+    lg: "max-w-6xl",
+    xl: "max-w-7xl",
+    default: "max-w-5xl",
+    full: "max-w-full",
+  };
+
+  return (
+    <div
+      className={`
+        w-full px-4 sm:px-6 lg:px-8
+        ${centered ? "mx-auto" : ""}
+        ${sizes[size]}
+        ${className}
+      `}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
