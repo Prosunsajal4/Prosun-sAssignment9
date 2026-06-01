8bd0106099e9db37e7a338e9be63aa249f54f37e
Prosunsajal4 <prosunsajal123@gmail.com>
2026-06-01 20:09:03 +0600

    docs: add JSDoc comments to Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,6 +1,17 @@
 import { Link as RouterLink } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
+/**
+ * Link component - Navigation link with internal/external support
+ * @component
+ * @param {Object} props - Component props
+ * @param {React.ReactNode} props.children - Link text/content
+ * @param {string} props.to - Link destination URL or route
+ * @param {boolean} [props.external=false] - Open in new tab if external
+ * @param {boolean} [props.underline=false] - Show underline decoration
+ * @param {string} [props.className=""] - Additional CSS classes
+ * @returns {React.ReactElement} Link element
+ */
 const Link = ({
   children,
   to,
